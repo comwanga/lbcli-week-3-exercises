7489692634199e790805cf162a96fb2febd8dd1f
comwanga <mwanga02717@gmail.com>
2026-07-08 12:39:31 +0300

    Complete week 3 submissions

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,3 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+echo ea73a631b456d2b041ed73bf5767946408c6ff067716929a68ecda2e3e4de6d3
